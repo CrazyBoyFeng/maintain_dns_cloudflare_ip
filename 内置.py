aefa93d8993cab9164f41a5e661a52300dabf040
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-09 00:30:29 +0800

    域名解析记录接口

--- 内置.py
+++ 内置.py
@@ -1,11 +1,13 @@
 # 咱试试用中文写代码，反正开发者和用户都是中文使用者。
 # 这样可以不用写注释，沟通效率也更高。
+
 from builtins import str as 字符串
 from configparser import ConfigParser as 配置解析
 from http.client import HTTPResponse as Http答复
 from urllib.request import Request as 网络请求
 
 字符串.小写 = 字符串.lower
+字符串.编码 = 字符串.encode
 
 配置解析.读取 = 配置解析.read
 配置解析.取 = 配置解析.get
