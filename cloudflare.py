e56b11aed8d4baa1dd072548382666cb976b0a9e
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-08 20:06:28 +0800

    Pacal命名

--- cloudflare.py
+++ cloudflare.py
@@ -1,6 +1,6 @@
 from 内置 import *
 from urllib.request import urlopen as 打开网址
-from urllib.error import HTTPError as HTTP错误
+from urllib.error import HTTPError as Http错误
 from builtins import bool as 判断
 from builtins import int as 整数
 
@@ -11,6 +11,6 @@ def 检测有效(ip: 字符串) -> 判断:
         答复 = 打开网址(请求, 无, 5)
         结果 = 整数(答复.状态) is 204
         return 结果
-    except HTTP错误:
+    except Http错误:
         return 否
     pass
