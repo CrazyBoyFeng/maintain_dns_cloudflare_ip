0d7ae3dee2643cec7fc86ba32c271f7cad9088da
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-09 21:57:05 +0800

    优化时间输出

--- 维护.py
+++ 维护.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 from builtins import ValueError as 结果错误
-from builtins import print as 输出
+from builtins import print as 提示
 from builtins import str as 字符串
-from time import asctime as 标准时间
-from time import localtime as 本地时间
 from time import sleep as 挂起
-from time import time as 时间
+from datetime import datetime as 日期时间
 import dns
 import cloudflare
 import ipv4
@@ -28,7 +26,7 @@ class 配置项:
 
 
 def 域名解析服务(解析服务: 字符串, 账户: 字符串, 密码: 字符串, 域名: 字符串, 域名记录: 字符串):
-    输出('解析服务', 解析服务)
+    提示('解析服务', 解析服务)
     if 'DNSPod' in 解析服务:
         return dns.DNSPod(账户, 密码, 域名, 域名记录)
     # if '阿里' in 解析服务:
@@ -40,32 +38,31 @@ def 选取ip(ip: 字符串) -> 字符串:
     有效 = 否
     while not 有效:
         ip = ipv4.随机ip(ip)
-        输出('随机地址', ip)
+        提示('随机地址', ip)
         有效 = cloudflare.检测有效(ip)
-    输出('检测有效', ip)
+    提示('检测有效', ip)
     return ip
 
 
 def 维护(配置: 配置项):
     域名解析 = 域名解析服务(配置.dns, 配置.账户, 配置.密码, 配置.域名, 配置.域名记录)
     域名记录, ip = 域名解析.查询()
-    输出('域名记录', 域名记录)
-    输出('当前地址', ip)
+    提示('域名记录', 域名记录)
+    提示('当前地址', ip)
     if (not 配置.强制更新) and cloudflare.检测有效(ip):
         return
     ip = 选取ip(ip)
-    输出('修改地址', ip)
+    提示('修改地址', ip)
     域名解析.修改(ip)
 
 
 while 是:
-    现在 = 标准时间(本地时间(时间()))
-    输出("当前时间", 现在)
+    提示("当前时间", 日期时间.now())
     配置 = 配置项()
     try:
         维护(配置)
     except 结果错误 as 错误:
-        输出(错误)
-    输出(配置.间隔, '秒后将再次执行')
+        提示(错误)
+    提示(配置.间隔, '秒后将再次执行')
     挂起(配置.间隔)
-    输出()
+    提示()
