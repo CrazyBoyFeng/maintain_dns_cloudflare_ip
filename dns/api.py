bd425a2c01c63840d7a120098e740a28d53fe127
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-10 23:53:58 +0800

    删除无用接口声明

--- dns/api.py
+++ dns/api.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta as 抽象基类
 from abc import abstractmethod as 抽象方法
+from builtins import str as 字符串
 
 
 class 域名解析记录(metaclass=抽象基类):
@@ -7,9 +8,8 @@ class 域名解析记录(metaclass=抽象基类):
     def __init__(自身, 账户, 密码, 域名, 域名记录):
         pass
 
-    @抽象方法
-    def 查询(自身):
-        pass
+    域名记录: 字符串
+    ip: 字符串
 
     @抽象方法
     def 修改(自身, ip):
