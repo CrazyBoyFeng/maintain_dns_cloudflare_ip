ef821fac679f60f9fda4e95e3e09c51b0b123b52
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-11 13:51:45 +0800

    具体化接口声明

--- dns/api.py
+++ dns/api.py
@@ -5,12 +5,12 @@ from builtins import str as 字符串
 
 class 域名解析记录(metaclass=抽象基类):
     @抽象方法
-    def __init__(自身, 账户, 密码, 域名, 域名记录):
+    def __init__(自身, 账户: 字符串, 密码: 字符串, 域名: 字符串, 域名记录: 字符串):
         pass
 
     域名记录: 字符串
     ip: 字符串
 
     @抽象方法
-    def 修改(自身, ip):
+    def 修改(自身, ip: 字符串):
         pass
