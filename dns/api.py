f63403093f8bf6a79798e63c80eea5d9d5026d63
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-11 23:38:35 +0800

    类变量扩展为属性

--- dns/api.py
+++ dns/api.py
@@ -1,16 +1,20 @@
 from abc import ABCMeta as 抽象基类
 from abc import abstractmethod as 抽象方法
 from builtins import str as 字符串
+from builtins import property as 属性
 
 无 = None
 
 
 class 域名解析记录(metaclass=抽象基类):
     @抽象方法
-    def __init__(自身, 账户: 字符串, 密码: 字符串, 域名: 字符串, 域名记录: 字符串, ip: 字符串 = 无):
+    def __init__(自身, 账户: 字符串, 密码: 字符串, 主域名: 字符串, 子域名: 字符串 = 无, 线路ip: 字符串 = 无):
         pass
 
-    ip: 字符串
+    @属性
+    @抽象方法
+    def ip(自身) -> 字符串:
+        pass
 
     @抽象方法
     def 修改(自身, ip: 字符串):
