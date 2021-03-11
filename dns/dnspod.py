0091bcacb8e43a04a92dbd2bcf860f8df4ccc325
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-11 23:42:11 +0800

    规范主域名和子域名设置

--- dns/dnspod.py
+++ dns/dnspod.py
@@ -1,7 +1,8 @@
-from builtins import IndexError as 索引错误
 from builtins import ValueError as 结果错误
+from builtins import dict as 字典
 from builtins import str as 字符串
 from json import load as 载入json
+from urllib.parse import urlencode as url编码字典
 from urllib.request import Request as 网络请求
 from urllib.request import urlopen as 打开网址
 
@@ -10,55 +11,63 @@ from .api import 域名解析记录
 无 = None
 
 请求头部 = {
+    'Content-Type': 'application/x-www-form-urlencoded',
     'User-Agent': 'maintain_dns_cloudflare_ip/0.0.1 (crazyboyfeng@qq.com)'
 }
 
 
-def 请求结果(请求路径: 字符串, 参数: 字符串):
+def 请求结果(请求路径: 字符串, 参数: 字典):
     网址 = 'https://dnsapi.cn/' + 请求路径
-    数据 = 参数.encode('utf-8')
+    编码 = url编码字典(参数)
+    数据 = 编码.encode('utf-8')
     请求 = 网络请求(网址, 数据, 请求头部)
     答复 = 打开网址(请求)
     return 载入json(答复)
 
 
 class DNSPod(域名解析记录):
-    def __init__(自身, 账户: 字符串, 密码: 字符串, 域名: 字符串, 域名记录: 字符串, ip: 字符串 = 无):
-        super().__init__(账户, 密码, 域名, 域名记录, ip)
-        自身.参数 = (
-                'login_token=%s,%s' % (账户, 密码)
-                + '&format=json'
-                + '&lang=cn'
-                + '&error_on_empty=no'
-                + '&domain=' + 域名
-                + '&sub_domain=' + 域名记录
-        )
-        自身.域名记录 = 域名记录
-        自身.ip = ip
+    def __init__(自身, 账户: 字符串, 密码: 字符串, 主域名: 字符串, 子域名: 字符串 = 无, 线路ip: 字符串 = 无):
+        super().__init__(账户, 密码, 主域名, 子域名, 线路ip)
+        自身.固定参数 = {
+            'login_token': '%s,%s' % (账户, 密码),
+            'format': 'json',
+            'lang': 'cn',
+            'error_on_empty': 'yes',
+            'domain': 主域名,
+            'sub_domain': 子域名 or '@',
+        }
+        自身.记录 = {
+            'value': 线路ip
+        }
         自身.查询()
         pass
 
     def 查询(自身):
-        参数 = 自身.参数 + '&length=1' + '&record_type=A'
-        if 自身.ip:
-            参数 += '&keyword=' + 自身.ip
+        参数 = 自身.固定参数.copy()
+        参数['length'] = '1'
+        参数['record_type'] = 'A'
+        if 自身.记录['value']:
+            参数['keyword='] = 自身.记录['value']
         json = 请求结果('Record.List', 参数)
-        if json['status']['code'] != '1':
+        if json['status']['code'] is not '1':
             raise 结果错误(json['status']['message'])
-        try:
-            记录 = json['records'][0]
-        except 索引错误:
-            raise 结果错误('域名记录 查询失败')
-        if 'record' not in 自身.参数:
-            自身.参数 += '&record_id=' + 记录['id']
-            自身.参数 += '&record_line_id=' + 记录['line_id']
-        自身.ip = 记录['value']
+        记录 = json['records'][0]
+        自身.记录 = {
+            'record_id': 记录['id'],
+            'record_line_id': 记录['line_id'],
+            'value': 记录['value']
+        }
         pass
 
+    def ip(自身) -> 字符串:
+        return 自身.记录['value']
+
     def 修改(自身, ip):
-        参数 = 自身.参数 + '&value=' + ip
+        参数 = 自身.固定参数.copy()
+        参数.update(自身.记录)
+        参数['value'] = ip
         json = 请求结果('Record.Ddns', 参数)
-        if json['status']['code'] != '1':
+        if json['status']['code'] is not '1':
             raise 结果错误(json['status']['message'])
-        自身.ip = ip
+        自身.记录['value'] = ip
         pass
--- dns/阿里云.py
+++ dns/阿里云.py
@@ -41,80 +41,71 @@ def 请求(签名参数: 字典) -> 字典:
 
 
 class 阿里云(域名解析记录):
-    def 签名参数(自身, 参数字典: 字典) -> 字典:  # 计算签名,返回签名后的查询参数
-        公共参数字典 = {
-            'Format': 'json',
-            'Version': '2015-01-09',
-            'AccessKeyId': 自身.id,
-            'Timestamp': 日期时间.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-            'SignatureMethod': 'HMAC-SHA1',
-            'SignatureNonce': 字符串(uuid4随机数()),  # 时间戳或随机数有一个就够了，再加一个纯属脱裤子放屁
-            'SignatureVersion': '1.0'
-        }
-        参数字典.update(公共参数字典)
-        参数列表 = 排序(参数字典.items())  # 竟然还需要排序？
+    def 签名参数(自身, 操作: 字符串, 参数1: 字典[字符串, 字符串] = 无, **参数2: 字符串) -> 字典:  # 计算签名,返回签名后的查询参数
+        参数字典: 字典 = 自身.固定参数.copy()
+        参数字典.update(参数1)
+        参数字典.update(参数2)
+        参数字典['Action'] = 操作
+        参数字典['Timestamp'] = 日期时间.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        参数字典['SignatureNonce'] = 字符串(uuid4随机数())  # 时间戳或随机数有一个就够了，再加一个纯属脱裤子放屁
+        参数列表 = 排序(参数字典.items())
         编码参数 = url编码字典(参数列表, quote_via=url编码)  # ' ' 变 '%20'
-        签名请求 = (
-                'POST'  # 难道服务器不知道请求方式需要我特别告诉它？
+        签名请求 = ('POST'  # 难道服务器不知道请求方式需要我特别告诉它？
                 + '&' + url编码('/', '')  # %27
                 + '&' + url编码(编码参数, '')  # 其实可以不设置 safe 忽略，因为此处已经没有 '/'
-        ).encode()  # 对已经编码过的请求再次编码处理 '=&%' 又一次脱裤子放屁
-        签名密钥 = (自身.secret + '&').encode()  # 为什么要加个符号？
+                ).encode('utf-8')  # 对已经编码过的请求再次编码处理 '=&%' 又一次脱裤子放屁
+        签名密钥 = (自身.secret + '&').encode('utf-8')  # 为什么要加个符号？
         签名摘要 = hmac消息验证(签名密钥, 签名请求, sha1).digest()  # 摘要
-        签名 = base64编码(签名摘要).strip()
-        参数字典['Signature'] = 签名
+        参数字典['Signature'] = base64编码(签名摘要).strip()
         return 参数字典  # 其实后面发送的时候还得再编一次码
 
-    def 固定参数(自身, 操作: 字符串, **参数) -> 字典:
-        参数['Action'] = 操作
-        参数['Lang'] = 'zh'
-        参数['DomainName'] = 自身.域名
-        return 参数
-
-    def __init__(自身, 账户: 字符串, 密码: 字符串, 域名: 字符串, 域名记录: 字符串, ip: 字符串 = 无):
-        super().__init__(账户, 密码, 域名, 域名记录, ip)
-        自身.id = 账户
+    def __init__(自身, 账户: 字符串, 密码: 字符串, 主域名: 字符串, 子域名: 字符串 = 无, 线路ip: 字符串 = 无):
+        super().__init__(账户, 密码, 主域名, 子域名, 线路ip)
+        自身.固定参数 = {
+            'Format': 'json',
+            'Version': '2015-01-09',
+            'AccessKeyId': 账户,
+            'SignatureMethod': 'HMAC-SHA1',
+            'SignatureVersion': '1.0',
+            'DomainName': 主域名,
+            'Lang': 'zh',
+            'Type': 'A'
+        }
         自身.secret = 密码
-        自身.域名 = 域名
-        自身.域名记录 = 域名记录
-        自身.ip = ip
-        自身.RR = 无
-        自身.RecordId = 无
-        自身.Type = 无
+        自身.记录 = {
+            'RR': 子域名 or '@',
+            'Value': 线路ip
+        }
         自身.查询()
         pass
 
     def 查询(自身):
-        if 自身.ip:
-            参数 = 自身.固定参数(
-                'DescribeDomainRecords',
-                ValueKeyWord=自身.ip,
-            )
-        else:
-            参数 = 自身.固定参数(
-                'DescribeSubDomainRecords',
-                SubDomain=自身.域名记录
-            )
-        参数['Type'] = 'A'
-        json = 请求(自身.签名参数(参数))
+        参数 = 自身.签名参数(
+            'DescribeDomainRecords',
+            KeyWord=自身.记录['Value'],
+            RRKeyWord=自身.记录['RR']
+        ) if 自身.记录['Value'] else 自身.签名参数(
+            'DescribeDomainRecords',
+            SubDomain=自身.记录['RR'] + '.' + 自身.固定参数['DomainName']
+        )
+        json = 请求(参数)
         try:
             记录 = json['DomainRecords']['Record'][0]
         except 索引错误:
-            raise 结果错误('域名记录 查询失败')
-        自身.RR = 记录['RR']
-        自身.RecordId = 记录['RecordId']
-        自身.Type = 记录['Type']
-        自身.ip = 记录['Value']
+            raise 结果错误('子域名 查询失败')
+        自身.记录 = {
+            'RR': 记录['RR'],
+            'RecordId': 记录['RecordId'],
+            'Type': 记录['Type'],
+            'Value': 记录['Value']
+        }
         pass
 
+    def ip(自身) -> 字符串:
+        return 自身.记录['Value']
+
     def 修改(自身, ip: 字符串):
-        参数 = 自身.固定参数(
-            'UpdateDomainRecord',
-            RR=自身.RR,
-            RecordId=自身.RecordId,
-            Type=自身.Type,
-            Value=ip
-        )
-        请求(自身.签名参数(参数))
-        自身.ip = ip
+        参数 = 自身.签名参数('UpdateDomainRecord', 自身.记录, Value=ip)
+        请求(参数)
+        自身.记录['Value'] = ip
         pass
