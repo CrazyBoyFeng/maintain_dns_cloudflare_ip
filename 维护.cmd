e7c9f21ddf80453bffad941184401e1437792c03
狂男风 <CrazyBoyFeng@Live.Com>
2021-03-19 01:42:45 +0800

    删除中文提高编码兼容性

--- 维护.cmd
+++ 维护.cmd
@@ -1,15 +1,15 @@
 @Echo off
 :: Windows Python 脚本启动器
-:: 结束后会暂停以查看错误信息
+:: 结束后会暂停以便查看错误信息
 SetLocal EnableDelayedExpansion
 Title %~n0
 CD /D "%~dp0"
 
 Python %~n0.py
-Set 退出码=!ErrorLevel!
+Set ExitCode=!ErrorLevel!
 Echo.
-If Not "!退出码!"=="0" (
-    Echo 退出码：!退出码!  1>&2
+If Not "!ExitCode!"=="0" (
+    Echo ExitCode: !ExitCode!  1>&2
 )
 Pause
-Exit !退出码!
+Exit !ExitCode!
